8c98d4ae9dd3e248f02c23f7d27d40ca5a2ee454
marisela <mariselaalejandragomez@gmail.com>
2025-03-24 10:30:04 -0700

    comment to test git/ git pull and push

--- play_tester.py
+++ play_tester.py
@@ -7,7 +7,7 @@ from tests.test_constants import *
 # import "pretty-print" library
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
-
+# comment to test git/ git pull and push
 # play testing section
 print("\n-----Wave 01 test data-----")
 pp.pprint(HORROR_1)
@@ -17,13 +17,13 @@ pp.pprint(FANTASY_2)
 # print("\n-----Wave 02 user_data-----")
 # pp.pprint(clean_wave_2_data())
 
-#print("\n-----Wave 03 user_data-----")
-#pp.pprint(clean_wave_3_data())
+# print("\n-----Wave 03 user_data-----")
+# pp.pprint(clean_wave_3_data())
 
 # Wave 04 user data
-#print("\n-----Wave 04 user_data-----")
-#pp.pprint(clean_wave_4_data())
+# print("\n-----Wave 04 user_data-----")
+# pp.pprint(clean_wave_4_data())
 
 # Wave 05 user data
-#print("\n-----Wave 05 user_data-----")
-#pp.pprint(clean_wave_5_data())
+# print("\n-----Wave 05 user_data-----")
+# pp.pprint(clean_wave_5_data())
