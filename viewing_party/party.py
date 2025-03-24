a4b6d2bed94cab97eac6d6baeedac5ec9b926cc7
marisela <mariselaalejandragomez@gmail.com>
2025-03-24 10:58:55 -0700

    wave 1 tests 1-5 completed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,28 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
 
+    if not title or not genre or not rating:
+        return None
+    new_movie = {"title": title, "genre": genre, "rating": rating}
+    return new_movie
+
+
+def add_to_watched(user_data, movie):
+    # print("AAAAAAAAAAA")
+
+    user_data["watched"].append(movie)
+
+    return user_data
+
+
+# use_data = {"watched": []}
+# movie = {
+#     "title": "MOVIE_TITLE_1",
+#     "genre": "GENRE_1",
+#     "rating": "RATING_1"
+# }
+# print(add_to_watched(use_data, movie))
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
@@ -12,7 +32,7 @@ def create_movie(title, genre, rating):
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
-        
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
@@ -20,4 +40,3 @@ def create_movie(title, genre, rating):
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
-
