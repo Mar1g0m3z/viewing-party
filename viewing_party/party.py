4de5c3cd2d17a2fb890cf59535365e3ac31cbd7f
Malik Elmessiry <malik.messiry@gmail.com>
2025-04-03 19:29:16 +0200

    commented another option

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -63,8 +63,6 @@ def get_watched_avg_rating(user_data):
 def get_most_watched_genre(user_data):
     if not user_data["watched"]:
         return None
-
-    genre_counts = {}
     
     genre_counts = {}  
     top_genre = None   
@@ -73,7 +71,7 @@ def get_most_watched_genre(user_data):
     for movie in user_data["watched"]:
         genre = movie["genre"]
 
-        if genre in genre_counts:     # can do this instead: genre_counts[genre] = genre_counts.get(genre, 0) + 1
+        if genre in genre_counts:  # for lines 74-77, can do this instead: genre_counts[genre] = genre_counts.get(genre, 0) + 1
             genre_counts[genre] += 1
         else:
             genre_counts[genre] = 1
@@ -83,7 +81,6 @@ def get_most_watched_genre(user_data):
             top_genre = genre
 
     return top_genre
-
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
